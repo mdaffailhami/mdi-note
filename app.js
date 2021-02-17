816950209f223eee59c1cb8b3037429a655a93b5
Daffa Ilhami <mdaffailhami@gmail.com>
2021-02-17 09:42:07 +0700

    Hapus else di next()

--- app.js
+++ app.js
@@ -18,9 +18,8 @@ app.use((req, res, next) => {
     if (req.headers["x-forwarded-proto"] != "https") {
       res.redirect(`https://${req.headers.host}${req.url}`);
     }
-  } else {
-    next();
   }
+  next();
 });
 
 // Session
